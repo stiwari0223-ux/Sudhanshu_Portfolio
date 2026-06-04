c2045ce2a32bc725f7dc7237c1ce69fdde2de62d
stiwari0223-ux <stiwari0223@gmail.com>
2026-06-04 14:00:44 +0530

    Update Navbar.jsx

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 const navLinks = [
   {href: "#about", label: "About"},
   {href: "#projects", label: "Projects"},
+  { href: "#experience", label: "Experience" },
 ];
 
 export const Navbar = () => {
